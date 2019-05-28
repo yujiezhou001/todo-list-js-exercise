d28aa1fa84c448e38462949138380afc31237abc
Gordon <gordonzhou68@outlook.com>
2019-05-28 17:18:17 +0000

    object oriented task completed, and code refactored

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// Object to keep track of each task's state
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+    markCompleted: function() {
+      this.complete = true;
+    },
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`)
+    }
+  };
+  return task;
 }
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
+const task2 = newTask("Do Laundry", "😨"); // task 1
+const task3 = newTask("Change Lightbulb", "\ud83d\udc0e\ud83d\udc71\u2764"); //task 2
+const tasks = [task1, task2, task3];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
+
+
+
